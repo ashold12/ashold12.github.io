154ebc39d459b9c4c26304d7c7525b9e3d060b65
AlexShold <ashold12@gmail.com>
2021-06-05 16:41:30 -0500

    inital

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderWidth: {
+        6: '6px'
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: {
+          DEFAULT: '#F1F4FFFF'
+        }
+        gray: {
+          DEFAULT: '#A2A2A1FF'
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
