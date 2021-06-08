118eeef239f0845aa1d724ec4203aae9d6ce45ad
AlexShold <ashold12@gmail.com>
2021-06-07 22:20:20 -0500

    refactor skills section for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,32 @@
 const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      screens: {
+        'xs': {'min': '508px', 'max': '1023px'},
+        'sm':	'640px',
+        'md':	'768px',
+        'lg':	'1024px',
+        'xl':	'1420px',
+        '2xl':	'1536px'
+      },
       borderWidth: {
         6: '6px'
       },
       width: {
         200: '200px',
-        140: '140px',
+        134: '134px',
         40: '40px',
       },
       height: {
         40: '40px',
       },
       minWidth: {
-        140: '140px',
+        134: '134px',
       },
       colors: {
         transparent: 'transparent',
